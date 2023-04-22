ddd1f6b98417a3a828dddd1003fcadc3600bbd4f
panicattheT3KN0 <113651342+panicattheT3KN0@users.noreply.github.com>
2023-04-22 00:25:10 -0400

    Added Pokemon List Array
    Added 3 pokemon to an array, with varying types of information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello World');
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [{name: "gengar", height: 5, type: ["ghost", "poison"]},
+    {name: "drifloon", height: 1, type: ["ghost", "flying"]},
+    {name: "mimikyu", height: 1, type: ["ghost", "fairy"]}
+];
