cbc21ef93d446dd16be1709782ea09fd970384b9
panicattheT3KN0 <113651342+panicattheT3KN0@users.noreply.github.com>
2023-05-11 14:12:15 -0400

    added text to display in HTML
    Added loops to help display descriptors in the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-let pokemonList = [{name: "gengar", height: 5, type: ["ghost", "poison"]},
-    {name: "drifloon", height: 1, type: ["ghost", "flying"]},
-    {name: "mimikyu", height: 1, type: ["ghost", "fairy"]}
+let pokemonList = [{name: "Gengar", height: 4.11, type: ["ghost", "poison"]},
+    {name: "Drifloon", height: 1.4, type: ["ghost", "flying"]},
+    {name: "Mimikyu", height: 0.8, type: ["ghost", "fairy"]}
 ];
+
+for (let i = 0; i < pokemonList.length; i++){
+    document.write("<p>" + pokemonList[i].name + " " + "(" + "height: " + pokemonList[i].height + ")" + "</p>")
+    if (pokemonList[i].height < 1)
+        document.write("Wow, so tiny!");
+  };
